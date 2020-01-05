ddf469109660274a8ad31f96a2401b03f27f92d5
anash <59493066+anashsh@users.noreply.github.com>
2020-01-05 13:16:24 +0700

    add about us

---
+++ SP_Mata/about.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Naive Bayes - Penyakit Mata</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="css/bootstrap4/bootstrap.min.css" rel="stylesheet">
+  <link href="css/bootstrap3/bootstrap.min.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <div class="container">
+      <a class="navbar-brand" href="index.php">(o_o) Identifikasi Penyakit Mata dengan Algoritma Naive Bayes (o_o)</a>
+    </div>
+  </nav>
+
+  <!-- Page Content -->
+  <div class="container">
+
+    <div class="row">
+
+      <div class="col-lg-3">
+        <h3 class="my-4"></h3>
+        <div class="list-group">
+          <a href="index.php" class="list-group-item">Konsultasi</a>
+          <a href="penyakit.php" class="list-group-item">Data Penyakit</a>
+          <a href="gejala.php" class="list-group-item">Data Gejala</a>
+          <a href="dataset.php" class="list-group-item">Dataset</a>
+          <a href="about.php" class="list-group-item active">Tentang Kami</a>
+        </div>
+      </div>
+      <!-- /.col-lg-3 -->
+
+      <div class="col-lg-9">
+        <div class="card mt-4">
+          <form method="#" action="#">
+            <div class="panel panel-danger">
+                <div class="panel-heading">
+                    <center><h2>Tentang Kami</h2></center>
+                </div>
+                <div class="panel-body">
+                    <b><p>Nama Anggota Kelompok :</p></b>
+                    <table class="table table-striped table-bordered" cellspacing="0" width="100%">
+                        <thead>
+                            <tr>
+                                <th class="th-sm">No
+                                </th>
+                                <th class="th-sm">NIM
+                                </th>
+                                <th class="th-sm">Nama
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td>
+                                1
+                            </td>
+                            <td>
+                                17.11.1225
+                            </td>
+                            <td>
+                                Yaffida Daffa Kumara
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                2
+                            </td>
+                            <td>
+                                17.11.1226
+                            </td>
+                            <td>
+                                Edo Pras Setio
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                3
+                            </td>
+                            <td>
+                                17.11.1240
+                            </td>
+                            <td>
+                                Ahmad Nashruddin
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                4
+                            </td>
+                            <td>
+                                17.11.1256
+                            </td>
+                            <td>
+                                Fahmi Nova Putera Alfasera
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+          </form>
+          </div>
+        </div>
+        <!-- /.card -->
+
+      </div>
+      <!-- /.col-lg-9 -->
+
+    </div>
+
+  </div>
+  <!-- /.container -->
+
+  <!-- Footer -->
+  <footer class="py-5 bg-dark">
+    <div class="container">
+      <p class="m-0 text-center text-white">Copyright &copy; AI 2019</p>
+    </div>
+    <!-- /.container -->
+  </footer>
+
+</body>
+
+</html>
